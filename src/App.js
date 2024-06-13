2589a8024fa8ca1f52175575a712657ea7f0dd68
Konainejaz <036635quonainejaz@gmail.com>
2024-06-13 22:40:28 +0500

    u

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav className="navBar">
+          <div>
+            <ul>
+              <li><a href="/"><img className="logo" src="https://puit.vercel.app/assets/puit-mRhmiYOZ.svg" alt="" /></a>
+              </li>
+              <li><a href="/login" id="login">Login</a></li>
+            </ul>
+          </div>
+        </nav>
       </header>
-    </div>
+      <main>
+        <section>
+          <div className="homePage">
+            <h1 className="home">Home</h1>
+            <a href="/create" className="create">Create New</a>
+            <div className="tabs"><button className="active">All</button><button className="">Old Course</button><button
+              className="">New Course</button></div>
+            <div className="old-course grid" >
+              <div className="card">
+                <p>Old Course</p>
+                <div className="links">
+                  <h1>5th Semester</h1>
+                </div>
+              </div>
+              <div className="card">
+                <p>Old Course</p>
+                <div className="links">
+                  <h1>6th Semester</h1>
+                </div>
+              </div>
+              <div className="card">
+                <p>Old Course</p>
+                <div className="links">
+                  <h1>7th Semester</h1>
+                </div>
+              </div>
+              <div className="card">
+                <p>Old Course</p>
+                <div className="links">
+                  <h1>8th Semester</h1>
+                </div>
+              </div>
+            </div>
+
+            <div className="new-course grid">
+              <div className="card">
+                <p>New Course</p>
+                <div className="links">
+                  <h1>1st Semester</h1>
+
+                </div>
+              </div>
+              <div className="card">
+                <p>New Course</p>
+                <div className="links">
+                  <h1>2nd Semester</h1>
+
+                </div>
+              </div>
+              <div className="card">
+                <p>New Course</p>
+                <div className="links">
+                  <h1>3rd Semester</h1>
+
+                </div>
+              </div>
+              <div className="card">
+                <p>New Course</p>
+                <div className="links">
+                  <h1>4th Semester</h1>
+
+                </div>
+              </div>
+              <div className="card">
+                <p>New Course</p>
+                <div className="links">
+                  <h1>5th Semester</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
